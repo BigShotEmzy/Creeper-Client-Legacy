f8f6a66e0c79e396e4d455972bb4db6df6b3d460
Cephetir <tupoluboy@gmail.com>
2021-10-13 19:43:13 +0300

    Added modes to Sprint (MODES AREN'T WORKING IN GUI!)

--- src/main/java/me/spruce/creeperclient/module/modules/movement/Sprint.java
+++ src/main/java/me/spruce/creeperclient/module/modules/movement/Sprint.java
@@ -2,25 +2,36 @@ package me.spruce.creeperclient.module.modules.movement;
 
 import me.spruce.creeperclient.module.Category;
 import me.spruce.creeperclient.module.Module;
+import me.spruce.creeperclient.setting.ModeSetting;
 import me.spruce.creeperclient.util.MovementUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Sprint extends Module {
 
-    public Sprint(){
+    public ModeSetting mode = new ModeSetting("Mode:", "Legit", "Legit", "Rage");
+
+    public Sprint() {
         super("Sprint", "Makes the player sprint automatically.", Keyboard.KEY_J, Category.MOVEMENT);
+        addSettings(mode);
     }
 
     @Override
-    public void onDisable(){
+    public void onDisable() {
         Minecraft.getMinecraft().player.setSprinting(false);
+        KeyBinding.setKeyBindState(29, false);
     }
 
     @Override
-    public void update(){
-        if(MovementUtils.canSprint()){
-            Minecraft.getMinecraft().player.setSprinting(true);
+    public void update() {
+        switch (mode.getMode()) {
+            case "Legit": KeyBinding.setKeyBindState(29, true);
+            case "Rage": Minecraft.getMinecraft().player.setSprinting(true);
         }
     }
 }
