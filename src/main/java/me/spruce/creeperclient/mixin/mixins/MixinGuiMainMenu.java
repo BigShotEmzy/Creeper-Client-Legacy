52aff34768387a8a92964ca5cc58c28b3087cceb
Cephetir <tupoluboy@gmail.com>
2021-10-15 15:12:55 +0300

    FINALLY fixed mixins!!
    Signed-off-by: Cephetir <tupoluboy@gmail.com>

--- src/main/java/me/spruce/creeperclient/mixin/mixins/MixinGuiMainMenu.java
+++ src/main/java/me/spruce/creeperclient/mixin/mixins/MixinGuiMainMenu.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin({GuiMainMenu.class})
 public class MixinGuiMainMenu extends GuiScreen {
 
-    @Inject(method = {"drawScreen"}, at = {@At("TAIL")}, cancellable = true)
+    @Inject(method = "drawScreen", at = @At("TAIL"), cancellable = true)
     public void drawText(CallbackInfo ci){
         mc.fontRenderer.drawStringWithShadow("Creeper Client " + Client.version, 2, 2, -1);
     }
