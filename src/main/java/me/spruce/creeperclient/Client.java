aabf121adcb775bf9f333e89f33bb4637a6d8239
Cephetir <tupoluboy@gmail.com>
2021-10-17 15:45:00 +0300

    super ultra mega cape fix

--- src/main/java/me/spruce/creeperclient/Client.java
+++ src/main/java/me/spruce/creeperclient/Client.java
@@ -5,7 +5,6 @@ import me.spruce.creeperclient.command.CommandManager;
 import me.spruce.creeperclient.module.ModuleManager;
 import me.spruce.creeperclient.util.font.FontUtil;
 import me.zero.alpine.EventManager;
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -41,36 +40,25 @@ public class Client {
         firstSetup();
     }
 
-    public String user;
     private int ticks = 0;
     URL l_URL = null;
     URLConnection l_Connection = null;
-    BufferedReader l_Reader = null;
-    String uuid = null;
+    public BufferedReader l_Reader = null;
 
     @SubscribeEvent
-    public void onTick(TickEvent.ClientTickEvent event) throws IOException {
+    public void onTick(TickEvent.ClientTickEvent event) {
         ticks++;
         if (ticks < 600) return;
         ticks = 0;
 
         new Thread(() -> {
             try {
-                uuid = Minecraft.getMinecraft().getSession().getProfile().getId().toString();
-                //uuid = "f11aefb8-6e8e-4411-905a-1171f1ae992c";
-
                 l_URL = new URL("https://raw.githubusercontent.com/DaCreeperGuy/Creeper-Client/Main/data.txt");
                 l_Connection = l_URL.openConnection();
                 l_Connection.setRequestProperty("User-Agent",
                         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
 
                 l_Reader = new BufferedReader(new InputStreamReader(l_Connection.getInputStream()));
-
-                user = l_Reader.lines().filter(s -> {
-                    String[] ss = s.split(":");
-                    if (ss[0].equals(uuid)) return true;
-                    return false;
-                }).findFirst().orElse("NOTHING");
             } catch(IOException e) {
                 e.printStackTrace();
             }
@@ -78,20 +66,11 @@ public class Client {
     }
 
     private void firstSetup() throws IOException {
-        uuid = Minecraft.getMinecraft().getSession().getProfile().getId().toString();
-        //uuid = "f11aefb8-6e8e-4411-905a-1171f1ae992c";
-
         l_URL = new URL("https://raw.githubusercontent.com/DaCreeperGuy/Creeper-Client/Main/data.txt");
         l_Connection = l_URL.openConnection();
         l_Connection.setRequestProperty("User-Agent",
                 "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
 
         l_Reader = new BufferedReader(new InputStreamReader(l_Connection.getInputStream()));
-
-        user = l_Reader.lines().filter(s -> {
-            String[] ss = s.split(":");
-            if (ss[0].equals(uuid)) return true;
-            return false;
-        }).findFirst().orElse("NOTHING");
     }
 }
